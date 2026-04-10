29c07efbb9fce2eeac67b863f341c4d999446ce5
cybugg <timehinsamuel11@gmail.com>
2026-04-10 12:57:59 +0100

    fix(entrypoint): correct extension file naming for marketplace packaging

---
+++ out/extension.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const activate = (_context) => {
+    console.log("kove VS Code extenstion is active.");
+};
+exports.activate = activate;
+const deactivate = () => {
+    // cleanup
+};
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
