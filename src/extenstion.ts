ca43557cdee83f92d5c2dc1f3ee6a73e62a0261e
cybugg <timehinsamuel11@gmail.com>
2026-04-10 12:59:04 +0100

    fix(entrypoint): correct extension file naming for marketplace packaging

--- src/extenstion.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from "vscode";
-
-export const activate = (_context: vscode.ExtensionContext): void => {
-    console.log("kove VS Code extenstion is active.");
-}
-
-export const deactivate = (): void => {
-    // cleanup
-}
